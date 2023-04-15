847318ac04b1eb03ea6e8ee9bd7526633a30ae49
Jaewon Hwang <79994991+HwangJae-won@users.noreply.github.com>
2023-04-15 20:56:14 +0900

    [level 2] Title: 짝지어 제거하기, Time: 113.72 ms, Memory: 14.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,13 @@
+#stack 활용 문제!
+def solution(s):
+    stack = []
+    for i in range(len(s)):
+        if not stack:
+            stack.append(s[i])     
+        else:
+            if s[i] == stack[-1]:       
+                stack.pop()
+            else:
+                stack.append(s[i])      
+    if stack : return 0                
+    else : return 1        
