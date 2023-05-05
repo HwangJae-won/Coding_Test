48cfe549f243112635a74929800a8307d19af997
Jaewon Hwang <79994991+HwangJae-won@users.noreply.github.com>
2023-05-05 23:27:15 +0900

    [unrated] Title: 둘만의 암호, Time: 0.03 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -0,0 +1,10 @@
+def solution(s, skip, index):
+    answer = 'abcdefghijklmnopqrstuvwxyz'
+    result = []
+    for i in skip:
+        if i in answer:
+            answer = answer.replace(i, "") 
+    for j in s:
+        result.append(answer[(answer.index(j)+index) % len(answer)])
+            
+    return "".join(result)
