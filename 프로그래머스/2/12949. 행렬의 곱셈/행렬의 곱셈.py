a018885eda2730a926affbdadefbfa9dcf6611a9
Jaewon Hwang <79994991+HwangJae-won@users.noreply.github.com>
2024-01-07 23:06:36 +0900

    [level 2] Title: 행렬의 곱셈, Time: 5.74 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -0,0 +1,13 @@
+def solution(arr1, arr2):
+    x = len(arr1)
+    y = len(arr2[0])
+    # arr2_T = list(zip(*arr2)) 
+    arr2_T=[list(i) for i in zip(*arr2)]
+    answer = [[0]*y for i in range(x)]
+    # print(answer)
+    for i in range(x) :
+        for j in range(y) :  
+            answer[i][j] = sum([a*b for a,b in zip(arr1[i],arr2_T[j])])
+           
+            
+    return answer
