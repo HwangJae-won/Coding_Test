80f1c94121893d3873f79e1c3e7741976ea58a2e
Jaewon Hwang <79994991+HwangJae-won@users.noreply.github.com>
2024-04-04 16:16:03 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.43 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int angle) {
+    int answer = 0;
+    if (0<angle and angle <90)
+        answer=1;
+    if (angle ==90)
+        answer=2;
+    if (90<angle and angle<180)
+        answer = 3;
+    if (angle ==180)
+        answer = 4;
+    return answer;
+}
