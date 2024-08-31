e7879d527896c78d428020668b39f864a4f6b3d5
Jaewon Hwang <79994991+HwangJae-won@users.noreply.github.com>
2024-08-31 16:45:41 +0900

    [level 3] Title: 단어 변환, Time: 6.94 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,4 +1,5 @@
 from collections import deque  #BFS 구현 시에는 deque 활용 
+#중복이 없다는 제한 사항이 없다면 set을 통해 중복 처리를 해줘야 함
 
 def solution(begin, target, words):  
     #예외처리
@@ -7,23 +8,23 @@ def solution(begin, target, words):
     return bfs(begin, target, words)
 
 def bfs(begin, target, words):
-    #begin과 word ruddndml
     queue = deque()
     queue.append([begin, 0]) #시작 단어와 단계 0으로 초기화
     
-    while queue: #
+    while queue: 
         now, step = queue.popleft() #큐의 가장 첫번째 요소 꺼내기
         
         if now == target:
+            #시작 단어와 target이 같아지는 순간 지금까지 이동한 개수인 step 반환하기 
             return step
         
         #단어를 모두 체크하면서, 해당 단어가 변경될 수 있는지 체크
         for word in words:
             count = 0
-            for i in range(len(now)): #단어의 길이만큼 반복하여
+            #단어의 길이만큼 반복하여 (모든 단어의 길이는 동일하다는 조건 있음)
+            for i in range(len(now)):         
                 if now[i] != word[i]: #단어에 알파벳 한개씩 체크하기
-                    count += 1
-                    
+                    count += 1  #글자가 다를 경우 숫자 증가
             if count == 1: 
                 queue.append([word, step+1])
     return answer
