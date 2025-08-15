6e66e78fcd132d84e78febeacbee28b7a3279818
Jaewon Hwang <79994991+HwangJae-won@users.noreply.github.com>
2025-08-16 00:33:54 +0900

    Update 완주하지 못한 선수.py
    add only dictionary used code

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,3 +1,20 @@
+#Counter 없이 딕셔너리만 쓰기 
+def solution(participant, completion):
+    counts = {}
+    for name in participant:
+        if name in counts:
+            counts[name] += 1
+        else:
+            counts[name] = 1
+
+    for name in completion:
+        counts[name] -= 1
+        if counts[name] == 0:
+            del counts[name]
+
+    return list(counts.keys())[0]
+
+#Counter 사용
 from collections import Counter
 
 def solution(participant, completion):
