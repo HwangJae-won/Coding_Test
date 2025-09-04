436c4239aadd80c61909e8b868e52e4a03e05e0e
HwangJae-won <dch222@naver.com>
2025-09-04 23:43:34 +0900

    chore: clean up repository structure and remove unused files

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++
@@ -1,8 +0,0 @@
-#접두어 있는 경우 False / 그렇지 않으면 True
-def solution(phone_book):
-    #우선 정렬
-    phone_book.sort()
-    for i in range(len(phone_book)-1):
-        if phone_book[i+1].startswith(phone_book[i]):
-            return False
-    return True
