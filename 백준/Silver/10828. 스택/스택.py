a3c98fff59b18fd3a1af459d239e6eae54890fe2
Jaewon Hwang <79994991+HwangJae-won@users.noreply.github.com>
2025-09-04 23:31:48 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+n  = int(input())
+stack =[]
+for _ in range(n):
+    command = input().split()
+    if command[0] == "push":
+        stack.append(int(command[1]))
+    elif command[0] =="pop":
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop()) #stack.pop()은 가장 최근 리스트의 요소가 반환됨
+        
+    elif command[0] == "size":
+        print(len(stack))
+    elif command[0] == "empty":
+        if len(stack) ==0:
+            print(1)
+        else: 
+            print(0)
+    elif command[0] =="top":
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
